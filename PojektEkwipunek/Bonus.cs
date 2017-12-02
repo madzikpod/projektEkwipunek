ea3fd5a6f7a2cc4daecc3a8ad7811278986ea6af
Magdalena Podeszwa <maga.dzaga@gmail.com>
2017-12-02 17:17:40 +0000

    Dodanie Bonusa

---
+++ PojektEkwipunek/Bonus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektEkwipunek
+{
+    //właściwości ile i co zwiekszy przedmiot
+    public class Bonus
+    {
+        public double Premia { get; set; }
+        public StatystykiPostaci DoCzego { get; set; }
+    }
+    public enum StatystykiPostaci
+    {
+        Moc, Inteligencja, Obrona
+    }
+}
