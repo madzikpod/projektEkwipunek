4dad35f3dbb35abeb06cae7c9237ee6526ce08cd
Magdalena Podeszwa <maga.dzaga@gmail.com>
2017-12-02 17:19:27 +0000

    rozszerzenie wlsciwosci przedmiotu o wymagania i bonusy

--- PojektEkwipunek/Przedmiot.cs
+++ PojektEkwipunek/Przedmiot.cs
@@ -22,14 +22,35 @@ namespace ProjektEkwipunek
             this.Nazwa = nazwa;
             this.Waga = waga;
             this.Typ = typ;
+            this.Bonusy = new List<Bonus>();
+            this.Wlasciwosci = new List<string>();
+            this.Wymagania = new Dictionary<StatystykiPostaci, double>();
+        }
+
+        public Przedmiot(TypPrzedmiotu typ, string nazwa, double waga, List<Bonus> bonusy, List<string> wlasciwosci,
+            KtoMozeNosic ograniczenieKlasowe = KtoMozeNosic.Wszyscy,
+            Dictionary<StatystykiPostaci, double> wymagania = null) : this(typ, nazwa, waga)
+        {
+            this.Bonusy = bonusy;
+            this.Wlasciwosci = wlasciwosci;
+            this.OgraniczeniaKlasowe = ograniczenieKlasowe;
+            if (wymagania != null)
+            {
+                Wymagania = wymagania;
+            }
+
         }
         public Guid Id { get; private set; }
         public string Opis { get; set; }
         public TypPrzedmiotu Typ { get; private set; }
         public double Waga { get; private set; }
         public string Nazwa { get; private set; }
-        //wymagania
-        //właściwości
+        //wymagania co potrzeba aby zalozyc
+        public Dictionary<StatystykiPostaci, double> Wymagania { get; private set; }
+        //właściwości ile i co zwiekszy przedmiot
+        public List<string> Wlasciwosci { get; set; }
+        public List<Bonus> Bonusy { get; private set; }
+        public KtoMozeNosic OgraniczeniaKlasowe { get; private set; }
 
     }
 
@@ -37,4 +58,14 @@ namespace ProjektEkwipunek
     {
         Bron, Stroj, Buty, NakrycieGlowy
     }
+    [Flags]
+    public enum KtoMozeNosic
+    {
+        Wszyscy = 14,
+        Janusz = 1,
+        Grazyna = 2,
+        Sebix = 4,
+        Gimbus = 8
+    }
+
 }
